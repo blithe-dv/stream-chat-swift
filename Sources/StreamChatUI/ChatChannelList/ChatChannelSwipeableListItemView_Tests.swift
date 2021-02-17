6406a13d346d1718689611d44a50f53818a17d9e
Pavel Lopatin <mantergo@gmail.com>
2021-02-17 15:47:14 +0300

    Audit `ChatChannelSwipeableListItemView`

---
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelSwipeableListItemView_Tests.swift
@@ -0,0 +1,118 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatTestTools
+@testable import StreamChatUI
+import XCTest
+
+class ChatChannelSwipeableListItemView_Tests: XCTestCase {
+    func test_defaultAppearance() {
+        let view = ChatChannelSwipeableListItemView().withoutAutoresizingMaskConstraints
+        view.addSizeConstraints()
+        
+        // Simulate view moving to superview to have view initialized.
+        view.embedInSuperview()
+        
+        // Buttons are revealed after swipe gesture so we need to simulate it to check the buttons.
+        view.trailingConstraint?.constant = 50
+        
+        AssertSnapshot(view, variants: .onlyUserInterfaceStyles)
+    }
+    
+    func test_appearanceCustomization_usingUIConfig() {
+        var config = UIConfig()
+        config.colorPalette.alert = .green
+        
+        let view = ChatChannelSwipeableListItemView().withoutAutoresizingMaskConstraints
+        
+        view.uiConfig = config
+        view.addSizeConstraints()
+
+        // Simulate view moving to superview to have view initialized.
+        view.embedInSuperview()
+        
+        // Buttons are revealed after swipe gesture so we need to simulate it to check the buttons.
+        view.trailingConstraint?.constant = 50
+    
+        AssertSnapshot(view, variants: .onlyUserInterfaceStyles)
+    }
+    
+    func test_appearanceCustomization_usingAppearanceHook() {
+        class TestView: ChatChannelSwipeableListItemView {}
+        TestView.defaultAppearance {
+            $0.deleteButton.tintColor = .cyan
+        }
+
+        let view = TestView().withoutAutoresizingMaskConstraints
+        view.addSizeConstraints()
+        
+        // Simulate view moving to superview to have view initialized.
+        view.embedInSuperview()
+        
+        // Buttons are revealed after swipe gesture so we need to simulate it to check the buttons.
+        view.trailingConstraint?.constant = 50
+
+        AssertSnapshot(view, variants: .onlyUserInterfaceStyles)
+    }
+
+    func test_appearanceCustomization_usingSubclassing() {
+        class TestView: ChatChannelSwipeableListItemView {
+            override func setUpAppearance() {
+                deleteButton.tintColor = .cyan
+            }
+
+            override func setUpLayout() {
+                super.setUpLayout()
+                
+                let button = UIButton()
+                button.setImage(TestImages.yoda.image, for: .normal)
+                button.imageView?.contentMode = .scaleAspectFit
+                
+                NSLayoutConstraint.activate([
+                    button.heightAnchor.constraint(equalToConstant: 50),
+                    button.widthAnchor.constraint(equalToConstant: 50)
+                ])
+                
+                actionButtonStack.addArrangedSubview(button)
+            }
+        }
+
+        let view = TestView().withoutAutoresizingMaskConstraints
+        view.addSizeConstraints()
+
+        // Simulate view moving to superview to have view initialized.
+        view.embedInSuperview()
+        
+        // Buttons are revealed after swipe gesture so we need to simulate it to check the buttons.
+        view.trailingConstraint?.constant = 100
+        
+        AssertSnapshot(view, variants: .onlyUserInterfaceStyles)
+    }
+    
+    func test_deleteButtonHandler() {
+        let view = ChatChannelSwipeableListItemView()
+        
+        var deleteButtonTapped = false
+        view.deleteButtonAction = {
+            deleteButtonTapped = true
+        }
+        
+        // Trigger lifecycle methods for setting up the target-action for `deleteButton`.
+        view.embedInSuperview()
+        
+        view.deleteButton.simulateEvent(.touchUpInside)
+        
+        XCTAssert(deleteButtonTapped)
+    }
+}
+
+private extension ChatChannelSwipeableListItemView {
+    func addSizeConstraints() {
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 50),
+            widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+}
